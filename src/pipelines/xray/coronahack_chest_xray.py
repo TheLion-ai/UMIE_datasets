34358c5fbda6123af2bbdfe64efabe005f908860
Andrzej Komor <andrzejkomor@yahoo.com>
2023-11-27 16:00:33 +0100

    [Coronahack] Create pipeline - Added pipeline for Coronahack Dataset. - Changed 'add_new_ids.py' to skip images for whom ```img_id_extractor()``` returns None. - Changed 'create_file_tree.py' not to create masks folder, when 'mask_folder_name == None'. This is for classification datasets.

--- src/pipelines/xray/coronahack_chest_xray.py
+++ src/pipelines/xray/coronahack_chest_xray.py
@@ -1,5 +1,6 @@
 import os
 
+import pandas
 import yaml
 from sklearn.pipeline import Pipeline
 
@@ -15,7 +16,7 @@ def preprocess_coronahack_chest_xray(source_path: str, target_path: str) -> None
     """Preprocess the Coronahack Chest X-Ray dataset.
 
     This function preprocesses the Coronahack Chest X-Ray dataset. It converts the JPEG
-    images to PNG format.
+    images to PNG format, and adds lavels to file names.
 
     Args:
         source_path (str): path to the downloaded dataset. Location of the root archive folder
@@ -30,24 +31,58 @@ def preprocess_coronahack_chest_xray(source_path: str, target_path: str) -> None
         dataset_name
     ]
 
+    images_path = os.path.join(source_path, "Coronahack-Chest-XRay-Dataset")
+
+    # Read metadata csv
+    metadata_csv_path = os.path.join(source_path, "Chest_xray_Corona_Metadata.csv")
+    metadata = pandas.read_csv(metadata_csv_path)
+    metadata.rename(columns={"Unnamed: 0": "id"}, inplace=True)
+
+    def get_img_id_coronahack(img_path: os.PathLike, add_label: bool) -> str | None:
+        img_name = os.path.split(img_path)[-1]
+        img_row = metadata.loc[metadata["X_ray_image_name"] == img_name]
+
+        if img_row.empty:
+            # File not present in csv
+            return None
+        else:
+            if not add_label:
+                # Used as study_id_extractor
+                return img_row["id"].values[0]
+            else:
+                # Create Labels
+                if img_row["Label"].values[0] == "Normal":
+                    label = "good"
+                elif img_row["Label"].values[0] == "Pnemonia":
+                    if img_row["Label_1_Virus_category"].values[0] == "bacteria":
+                        label = "PneumoniaBacteria"
+                    elif img_row["Label_1_Virus_category"].values[0] == "Virus":
+                        label = "PneumoniaVirus"
+                    else:
+                        return None
+                else:
+                    return None
+
+            return f'{img_row["id"].values[0]}_{label}.png'
+
     params = {
-        "source_path": source_path,
+        "source_path": images_path,
         "target_path": target_path,
         "dataset_name": dataset_name,
         "dataset_uid": dataset_uid,
+        "z-fill": 4,
         "phases": phases,
+        "mask_folder_name": None,
+        "img_id_extractor": lambda x: get_img_id_coronahack(x, True),
+        "study_id_extractor": lambda x: get_img_id_coronahack(x, False),
     }
 
     pipeline = Pipeline(
         steps=[
             ("get_file_paths", GetFilePaths(**params)),
-            # ("create_file_tree", CreateFileTree(**params)),
-            # ("add_new_ids", AddNewIds(**params)),
-            # ("convert_dcm2png", ConvertDcm2Png(**params)),
-            # ("create_masks_from_xml", CreateMasksFromXML(**params)),
-            # # Choose either to create blank masks or delete images without masks
-            # # ("create_blank_masks", CreateBlankMasks(**params)),
-            # ("delete_imgs_without_masks", DeleteImgsWithoutMasks(**params)),
+            ("create_file_tree", CreateFileTree(**params)),
+            # add_new_ids is used here to also add labels
+            ("add_new_ids", AddNewIds(**params)),
         ],
     )
 
