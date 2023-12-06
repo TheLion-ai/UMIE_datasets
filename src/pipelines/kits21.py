e335035c1a6905f8d9333bf72db1e6c5e16a7cca
a-huli <klaudel.b@gmail.com>
2023-12-06 17:45:37 +0100

    Comment KITS21 Pipeline

--- src/pipelines/kits21.py
+++ src/pipelines/kits21.py
@@ -8,6 +8,7 @@ from typing import Any
 import cv2
 import numpy as np
 
+from config import mask_encodings_config
 from src.constants import MASK_FOLDER_NAME
 from src.pipelines.base_pipeline import BasePipeline, DatasetArgs
 from src.steps.add_labels import AddLabels
@@ -24,7 +25,7 @@ from src.steps.recolor_masks import RecolorMasks
 class KITS21Pipeline(BasePipeline):
     """Preprocessing pipeline for KITS21 dataset."""
 
-    name: str = field(default="KITS21")
+    name: str = field(default="KITS21")  # dataset name used in configs
     steps: list = field(
         default_factory=lambda: [
             ("create_file_tree", CreateFileTree),
@@ -41,19 +42,22 @@ class KITS21Pipeline(BasePipeline):
     )
     dataset_args: DatasetArgs = DatasetArgs(
         zfill=2,
-        img_id_extractor=lambda x: os.path.basename(x).split("_")[-1],
+        # Image id is in the source file name after the last underscore
+        img_id_extractor=lambda x: os.path.basename(x).split("_")[-1],  #
+        # Study id is the folder name of all images in the study
         study_id_extractor=lambda x: os.path.basename((os.path.dirname(x))).split("_")[-1],
-        window_center=50,  # TODO: add to config
+        # Windows of abddominal cavity CTs
+        window_center=50,
         window_width=400,
-        img_dcm_prefix="imaging",
-        segmentation_dcm_prefix="segmentation",
+        img_dcm_prefix="imaging",  # prefix of the source image file names
+        segmentation_dcm_prefix="segmentation",  # prefix of the source mask file names
     )
 
     def get_label(
         cls,
         img_path: str,
         labels_list: list,
-        kidney_tumor_encoding: int = 1,  # = mask_encoding_config["kidney_tumor"],
+        kidney_tumor_encoding: int = 1,
         mask_folder_name: str = MASK_FOLDER_NAME,
     ) -> list:
         """Get label for the image.
@@ -69,14 +73,18 @@ class KITS21Pipeline(BasePipeline):
         mask_path = os.path.join(root_path, mask_folder_name, img_id)
         mask = cv2.imread(mask_path)
 
+        # Check if the mask contains the kidney tumor
         if kidney_tumor_encoding in np.unique(mask):
+            # Study id is between the second and third underscore in the target image id
             study_id_regex = re.match(r"^(?:[^_]*_){2}([^_]+)", img_id)
             study_id = (
                 study_id_regex.group(1) if study_id_regex is not None else None
             )  # Study id is between the second and third underscore
             labels = []
             for case in labels_list:
+                # Find the case with the matching study id
                 if case["case_id"] == f"case_{study_id}":
+                    # Remove underscores from the label
                     label = case["tumor_histologic_subtype"].replace("_", "")
                     labels.append(label)
                     break
@@ -86,6 +94,12 @@ class KITS21Pipeline(BasePipeline):
     def __post_init__(self) -> None:
         """Post initialization actions."""
         super().__post_init__()
+        kidney_tumor_encoding = mask_encodings_config[self.name]["kidney_tumor"]
+        # Load labels from the labels file
         self.labels_list = self.load_labels_from_path()
-        self.dataset_args.get_label = partial(self.get_label, labels_list=self.labels_list)
+        # Add get_label function to the dataset_args
+        self.dataset_args.get_label = partial(
+            self.get_label, kidney_tumor_encoding=kidney_tumor_encoding, labels_list=self.labels_list
+        )
+        # Update args with dataset_args
         self.args: dict[str, Any] = dict(**self.args, **asdict(self.dataset_args))
