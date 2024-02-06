8e0b086d96aebeac6f2a99b059803f3ecd70b080
A-Huli <klaudel.b@gmail.com>
2024-02-06 21:28:47 +0100

    Skip dataset if required masks_path or labels_path is not provided

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ for dataset in datasets:
     # Check whether source_path is defined
     if not dataset.args["source_path"]:
         print(f"{dataset.name} skipped, as no source path found.")
+    elif "labels_path" in dataset.args.keys() and dataset.args["labels_path"] == "":
+        print(
+            f"{dataset.name} skipped, as no labels path found. This dataset requires labels path to extract annotations."
+        )
+    elif "masks_path" in dataset.args.keys() and dataset.args["masks_path"] == "":
+        print(
+            f"{dataset.name} skipped, as no masks path found. This dataset requires masks path to extract annotations."
+        )
     else:
         print(dataset.name)
         pipeline = dataset.pipeline
