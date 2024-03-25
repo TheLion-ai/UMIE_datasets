06059f04a415ddda3b57b6d4ea6a37c1b6c32608
Piotr Frackowski <p.frackowski12191@gmail.com>
2024-03-25 17:41:51 +0100

    Updated test data

--- src/steps/create_blank_masks.py
+++ src/steps/create_blank_masks.py
@@ -46,7 +46,6 @@ class CreateBlankMasks(TransformerMixin):
             ),
             recursive=True,
         )
-        print(mask_paths)
         mask_names = [os.path.basename(mask) for mask in mask_paths]
         print("Creating blank masks...")
         for img_path in tqdm(X):
