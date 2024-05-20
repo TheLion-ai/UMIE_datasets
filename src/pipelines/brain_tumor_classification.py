8ca6cf57498157a18f19570273dce801015db573
Kacper Bober <kbober@protonmail.com>
2024-05-20 13:18:12 +0200

    config setup for initial pipeline step

--- src/pipelines/brain_tumor_classification.py
+++ src/pipelines/brain_tumor_classification.py
@@ -1,16 +1,26 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, asdict
+from typing import Any
 
-from src.pipelines.base_pipeline import BasePipeline
+from src.pipelines.base_pipeline import BasePipeline, DatasetArgs
+from src.steps.create_file_tree import CreateFileTree
 
 
 @dataclass
 class BrainTumorClassificationPipeline(BasePipeline):
     """Preprocessing pipeline for Brain Tumor Classification dataset."""
 
-    name: str = field(default="Brain_Tumor_Detection")  # dataset name used in configs
+    name: str = field(default="Brain_Tumor_Classification_MRI")  # dataset name used in configs
     steps: list = field(
         default_factory=lambda: [
+            ("create_file_tree", CreateFileTree),
         ]
+    )
+
+    dataset_args: DatasetArgs = field(
+        default_factory=lambda: DatasetArgs(
+            image_folder_name="Images",
+            mask_folder_name=None,
+        )
     )
 
     def get_label(self) -> list:
@@ -19,3 +29,4 @@ class BrainTumorClassificationPipeline(BasePipeline):
 
     def prepare_pipeline(self) -> None:
         """Post initialization actions."""
+        self.args: dict[str, Any] = dict(**self.args, **asdict(self.dataset_args))
