56279b8ba9683d7c0bc6b16a341536245c5d0c12
Mariusz <mariusz.kosakowski@outlook.com>
2024-12-23 12:51:50 +0100

    Typo bug and code refactoring

--- src/pipelines/brain_tumor_classification.py
+++ src/pipelines/brain_tumor_classification.py
@@ -10,6 +10,7 @@ from base.pipeline import BasePipeline, PipelineArgs
 from base.selectors.img_selector import BaseImageSelector
 from base.selectors.mask_selector import BaseMaskSelector
 from config.dataset_config import DatasetArgs, brain_tumor_classification
+from constants import IMG_FOLDER_NAME
 from steps import (
     AddLabels,
     AddUmieIds,
@@ -113,7 +114,7 @@ class BrainTumorClassificationPipeline(BasePipeline):
     dataset_args: DatasetArgs = field(default_factory=lambda: brain_tumor_classification)
     pipeline_args: PipelineArgs = field(
         default_factory=lambda: PipelineArgs(
-            image_folder_name="Images",
+            image_folder_name=IMG_FOLDER_NAME,
             img_id_extractor=ImgIdExtractor(),
             study_id_extractor=StudyIdExtractor(),
             img_prefix="",
