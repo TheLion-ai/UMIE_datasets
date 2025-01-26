bbfaadc35b8662267c3eab5e288a874fd420040b
Kamil Staszewski <kamil.staszewski@cloudflight.io>
2025-01-26 15:30:44 +0100

    feat(#106): add more information when images are not identical

--- testing/tests/test_kits23.py
+++ testing/tests/test_kits23.py
@@ -18,8 +18,12 @@ from testing.libs.dataset_testing_lib import DatasetTestingLibrary
 source_path = os.path.join(os.getcwd(), "testing/test_dummy_data/00_kits23/input")
 target_path = os.path.join(os.getcwd(), "testing/test_dummy_data/00_kits23/output")
 masks_path = os.path.join(os.getcwd(), "testing/test_dummy_data/00_kits23/input")
-labels_path = os.path.join(os.getcwd(), "testing/test_dummy_data/00_kits23/input/kits23.json")
-expected_output_path = os.path.join(os.getcwd(), "testing/test_dummy_data/00_kits23/expected_output")
+labels_path = os.path.join(
+    os.getcwd(), "testing/test_dummy_data/00_kits23/input/kits23.json"
+)
+expected_output_path = os.path.join(
+    os.getcwd(), "testing/test_dummy_data/00_kits23/expected_output"
+)
 
 
 def test_initial_clean_up_kits23():
@@ -49,22 +53,30 @@ def test_kits23_verify_file_tree():
     expected_file_tree = glob.glob(f"{str(expected_output_path)}/**", recursive=True)
     current_file_tree = glob.glob(f"{str(target_path)}/**", recursive=True)
 
-    if not DatasetTestingLibrary.verify_file_tree(expected_file_tree, current_file_tree):
+    if not DatasetTestingLibrary.verify_file_tree(
+        expected_file_tree, current_file_tree
+    ):
         pytest.fail("KITS23 pipeline created file tree different than expected.")
 
 
 def test_kits23_verify_images_correct():
     """Test to verify whether all images have contents as expected."""
-    expected_file_tree = glob.glob(f"{str(expected_output_path)}/**/*.png", recursive=True)
+    expected_file_tree = glob.glob(
+        f"{str(expected_output_path)}/**/*.png", recursive=True
+    )
     current_file_tree = glob.glob(f"{str(target_path)}/**/*.png", recursive=True)
 
-    if not DatasetTestingLibrary.verify_all_images_identical(expected_file_tree, current_file_tree):
+    if not DatasetTestingLibrary.verify_all_images_identical(
+        expected_file_tree, current_file_tree
+    ):
         pytest.fail("KITS23 pipeline created image contents different than expected.")
 
 
 def test_kits23_verify_jsonl_correct():
     """Test to verify whether all json files have contents as expected."""
-    expected_jsonl_path = glob.glob(f"{expected_output_path}/**/**.jsonl", recursive=True)[0]
+    expected_jsonl_path = glob.glob(
+        f"{expected_output_path}/**/**.jsonl", recursive=True
+    )[0]
     current_jsonl_path = glob.glob(f"{target_path}/**/**.jsonl", recursive=True)[0]
     with open(expected_jsonl_path, "r") as file:
         expected_jsonl = [json.loads(line) for line in file]
@@ -72,7 +84,9 @@ def test_kits23_verify_jsonl_correct():
         current_jsonl = [json.loads(line) for line in file]
 
     if not DatasetTestingLibrary.verify_jsonl_identical(expected_jsonl, current_jsonl):
-        pytest.fail("Brain Tumor Progression pipeline created jsonl contents different than expected.")
+        pytest.fail(
+            "Brain Tumor Progression pipeline created jsonl contents different than expected."
+        )
 
 
 def test_clean_up_kits23():
